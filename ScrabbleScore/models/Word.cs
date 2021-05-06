8b3a930803dc7a6f73395d34f1e9f2a07a3fc019
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-05-06 14:17:22 -0700

    passed our fifth test
    Co-authored-by: jonathanstull <jonathanstull@users.noreply.github.com>

--- ScrabbleScore/models/Word.cs
+++ ScrabbleScore/models/Word.cs
@@ -19,7 +19,20 @@ namespace Scrabble
 
     public int ScoreAssign(char letter)
     {
-      return letter;
+      // instantiate key array for scores
+      // loop that compares the input letter to the key array and increments a score variable
+      // return the int value once the loop is complete
+      char[] worthTen = { 'q', 'z' };
+      int score = 0;
+      int i = 0;
+
+      for (i = 0; i < worthTen.Length; i++)
+        // in JS, we could use Array.includes() to see if an element is there
+        if (letter == worthTen[i])
+        {
+          score = score + 10;
+        }
+      return score;
     }
 
   }
